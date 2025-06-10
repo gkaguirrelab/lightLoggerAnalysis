d337b63fd7210046e847a5f9ef6d588f449e2e36
gkaguirre <gkaguirre@me.com>
2025-06-10 08:52:56 -0400

    Editing the header example

--- code/libraries_matlab/plotSPD.m
+++ code/libraries_matlab/plotSPD.m
@@ -1,4 +1,4 @@
-function plotSPDnew(spd, frq)
+function plotSPD(spd, frq)
 % Plots the SPD function for a chunk.
 %
 % Syntax:
@@ -14,16 +14,15 @@ function plotSPDnew(spd, frq)
 %
 % Examples:
 %{  
-%   test_path = "./mychunks";
-%   chunks = parse_chunks(test_path);
-%   [spd, frq] = calcTemporalSPD(chunks{1}.W.v, 200);
-%   PlotSPD(spd, frq);
+    test_path = "./mychunks";
+    chunks = parse_chunks(test_path);
+    [spd, frq] = calcTemporalSPD(chunks{1}.W.v, 200);
+    PlotSPD(spd, frq);
 %} 
 
 arguments 
         spd (1, :)
         frq (1, :)
-       
 end
 
 % Plot and label
