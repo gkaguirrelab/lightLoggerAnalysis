0c7b447bc7acbb2aa79bad85414778dbf9a67786
gkaguirre <gkaguirre@me.com>
2025-06-23 13:58:55 -0400

    Make the selection of the data column an optional passed variable

--- code/libraries_matlab/data_analysis/extractLabSphereData.m
+++ code/libraries_matlab/data_analysis/extractLabSphereData.m
@@ -1,27 +1,27 @@
-function [meta, signal] = extractLabSphereData(filename)
+function [meta, signal] = extractLabSphereData(filename,sel)
 % Loads a Labsphere CSV, prompts user to pick a timestamp by number,
 % and returns key metadata fields in a struct plus time-series data for that scan.
 %
 % Syntax:
 %   [meta, signal] = extractLabSphereData('Labsphere_SaveAllScans.csv');
 %
-% Description: 
-%   This function loads the CSV data from one or more Labsphere scan(s), 
+% Description:
+%   This function loads the CSV data from one or more Labsphere scan(s),
 %   lists their available timestamps, and prompts the user for a selection.
-%   Extracts sample rate, frequency, and flicker metrics into the output 
-%   struct. Also reads the full time-series data for the 
+%   Extracts sample rate, frequency, and flicker metrics into the output
+%   struct. Also reads the full time-series data for the
 %   chosen scan and converts into a numeric 'signal' vector to be used in
 %   determining temporal SPD in units of contrast.
 %
 % Inputs:
-%   filename            - String. CSV file containing Labsphere scan data.  
+%   filename            - String. CSV file containing Labsphere scan data.
 %
 % Outputs:
 %   meta                - Struct. The desired metadata contained in the CSV
-%                         file. This can be manipulated to include/exclude 
-%                         specific metadata. 
-%   signal              - Numeric vector. 
-% 
+%                         file. This can be manipulated to include/exclude
+%                         specific metadata.
+%   signal              - Numeric vector.
+%
 % Examples:
 %{
     [meta, signal] = extractLabSphereData('Labsphere_SaveAllScans.csv');
@@ -48,19 +48,21 @@ stamps = string(rawData(2,:));
 comments = string(rawData(8,:));
 
 % Display scans with timestamp and comment
-fprintf('Available scans:\n');
-for i = 1:nScans
-    if strlength(comments(i)) > 0
-        fprintf('  %2d: %s  — %s\n  ', i, char(stamps(i)), char(comments(i)));
-    else
-        fprintf('  %2d: %s\n  ', i, char(stamps(i)));
+if nargin == 1
+    fprintf('Available scans:\n');
+    for i = 1:nScans
+        if strlength(comments(i)) > 0
+            fprintf('  %2d: %s  — %s\n  ', i, char(stamps(i)), char(comments(i)));
+        else
+            fprintf('  %2d: %s\n  ', i, char(stamps(i)));
+        end
     end
-end
 
-% Prompt user to select scan number
-sel = input(sprintf('Enter scan number (1–%d): ', nScans));
-if ~isscalar(sel) || sel<1 || sel>nScans || sel~=round(sel)
-  error('Invalid scan number. Must be an integer between 1 and %d.', nScans);
+    % Prompt user to select scan number
+    sel = input(sprintf('Enter scan number (1–%d): ', nScans));
+    if ~isscalar(sel) || sel<1 || sel>nScans || sel~=round(sel)
+        error('Invalid scan number. Must be an integer between 1 and %d.', nScans);
+    end
 end
 
 % Initialize output struct and extract desired metadata values
@@ -71,15 +73,15 @@ meta.PercentFlicker = rawData{30, sel};
 % add scale max value (row 12)
 
 % Extract time series data and convert into signal vector
-  seriesCells = rawData(37:end, sel);
-  N = numel(seriesCells);
-  signal = zeros(N,1);
-  for i = 1:N
+seriesCells = rawData(37:end, sel);
+N = numel(seriesCells);
+signal = zeros(N,1);
+for i = 1:N
     raw = seriesCells{i};
     signal(i) = raw;
-  end
+end
 
-% Trim trailing NaN values for shorter scans. 
+% Trim trailing NaN values for shorter scans.
 validIdx = find(~isnan(signal));
 signal = signal(1:validIdx(end));
 signal = (signal - mean(signal))/mean(signal);
