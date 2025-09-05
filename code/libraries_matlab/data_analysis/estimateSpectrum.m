68c6f44eea57759fc398c7e0ba5b036728b4f487
SamanthaAMontoya <155659827+SamanthaAMontoya@users.noreply.github.com>
2025-09-05 15:32:05 -0400

    updated spectrum estimation code

--- code/libraries_matlab/data_analysis/estimateSpectrum.m
+++ code/libraries_matlab/data_analysis/estimateSpectrum.m
@@ -31,7 +31,7 @@ S = (rand(1,8)*N)';
 S = S-min(S)+1;
 
 % Load a chunk of data
-chunk = load('/Users/samanthamontoya/Aguirre-Brainard Lab Dropbox/Sam Montoya/FLIC_data/lightLogger/sophia_in_wild_converted_for_sam/chunk_0.mat');
+chunk = load('/Users/samanthamontoya/Aguirre-Brainard Lab Dropbox/Sam Montoya/FLIC_data/lightLogger/sophia_in_wild_converted_for_sam/chunk_0.mat').chunk;
 wMS = chunk.M.v.AS(end,1:8)';
 tMS = chunk.M.t.AS(end);
 
@@ -67,6 +67,7 @@ wCam = wCam / max(wCam);
 [tCamDiff, tCamidx] = min(abs(tMS - chunk.W.t));
 tCam = chunk.W.t(tCamidx);
 vCam = chunk.W.v(tCamidx,:,:);
+vCam = squeeze(vCam);
 
 % make sure this is a decent frame (i.e., not washed out or too dark) 
 minCam = min(vCam, [], "all");
@@ -74,11 +75,46 @@ maxCam = max(vCam, [], "all");
 
 % what are we looking at?
 figure;
-imshow(squeeze(vCam), [0, 255]);
+imshow(vCam, [0, 255]);
+resCam = size(vCam);
 
 % retreive RGB values from the world camera
+[rows, cols] = ndgrid(1:resCam(1), 1:resCam(2));
+
+R_mask = mod(rows, 2) == 0 & mod(cols, 2) == 0;
+G_mask = (mod(rows, 2) == 0 & mod(cols, 2) == 1) | ...
+         (mod(rows, 2) == 1 & mod(cols, 2) == 0);
+B_mask = mod(rows, 2) == 1 & mod(cols, 2) == 1;
+
+WORLD_RGB_MASK(R_mask) = 1;  % R=1
+WORLD_RGB_MASK(G_mask) = 2;  % G=2
+WORLD_RGB_MASK(B_mask) = 3;  % B=3
+
+% calculate mean pixel intensity for RGB and multiply it by the channel
+% weights
+% RGB scalar multipliers
+WORLD_RGB_SCALARS = [1, 0.8223775, 0.95367937]; %from Zach
+%WORLD_RGB_SCALARS = [1.0000, 0.6190, 0.8095]; % from Pagnutti et al., 2017
+R_mean = mean(vCam(R_mask))*WORLD_RGB_SCALARS(1);
+G_mean = mean(vCam(G_mask))*WORLD_RGB_SCALARS(2);
+B_mean = mean(vCam(B_mask))*WORLD_RGB_SCALARS(3);
+
+maxRGB = max([R_mean, G_mean, B_mean]);
+BGR_norm = [B_mean/maxRGB; G_mean/maxRGB; R_mean/maxRGB];
 
 
+% Create empty 3D RGB image
+rgbImage = zeros(resCam(1), resCam(2), 3);
+
+% Assign pixels to their corresponding color channels
+rgbImage(:,:,1) = double(vCam).*R_mask.*WORLD_RGB_SCALARS(1); % Red channel
+rgbImage(:,:,2) = double(vCam).*G_mask.*WORLD_RGB_SCALARS(2); % Green channel
+rgbImage(:,:,3) = double(vCam).*B_mask.*WORLD_RGB_SCALARS(3); % Blue channel
+
+% Display the image
+figure;
+imshow(rgbImage / 255); % Normalize to [0,1] for imshow
+title('Raw Bayer Pattern Visualization');
 
 
 
