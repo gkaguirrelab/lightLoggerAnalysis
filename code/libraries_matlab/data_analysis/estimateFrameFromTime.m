52a4d3e3c79e59779f79b941c42b0f3b4f7b1285
SamanthaAMontoya <155659827+SamanthaAMontoya@users.noreply.github.com>
2025-10-10 14:33:44 -0400

    updates to gaze cal for virtual foveation

--- code/libraries_matlab/data_analysis/estimateFrameFromTime.m
+++ code/libraries_matlab/data_analysis/estimateFrameFromTime.m
@@ -1,8 +1,6 @@
 fps = 120;
-totalTime = 129.37; %sec
+totalTime = 214.3; %sec
 dotTimeFromEnd = [39, 35, 32, 28, 25, 21, 18, 14, 11, 8];
-dotTimeFromEnd = 40.833;
+dotTimeFromEnd = 128.358;
 dotTimeSec = totalTime - dotTimeFromEnd; % seconds
 dotFrame = dotTimeSec.*fps;
-
-pupil_features{1,2}.data{dotFrame}
