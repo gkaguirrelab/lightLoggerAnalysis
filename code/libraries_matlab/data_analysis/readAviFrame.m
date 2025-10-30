8baa3559ea6310bad7af70f8969ee6a8e7b74d08
Zachary Kelly <zacharykelly@NXStudDesk86.asc.upenn.edu>
2025-10-30 12:30:26 -0400

    add functions but for some reason they are returning none

---
+++ code/libraries_matlab/data_analysis/readAviFrame.m
@@ -0,0 +1,18 @@
+function frame = readAviFrame(video_path, idx, options)
+    arguments 
+        video_path {mustBeText}; 
+        idx {mustBeNumeric}; 
+        options.grayscale {mustBeNumericOrLogical} = false; 
+    end 
+
+    % Only import Pi_util once
+    persistent Pi_util
+    if(isempty(Pi_util))
+        Pi_util = import_pyfile(getpref("lightLoggerAnalysis", "Pi_util_path")); 
+    end 
+
+    % Retrieve the frame from the video
+    frame = uint8(Pi_util.extract_frames_from_video(video_path, {idx}, options.grayscale));
+
+    return ; 
+end 
