051eec13e831ea7710163ddfa644d1d3c030633e
Zachary Kelly <zacharykelly@brb-00453.apn.wlan.private.upenn.edu>
2025-11-14 08:03:23 -0500

    clean MATLAB IO writer up a little bit

--- code/library/matlabIO/@videoIOWrapper/close.m
+++ code/library/matlabIO/@videoIOWrapper/close.m
@@ -7,10 +7,22 @@ function close(obj)
     temp_dir_path = fullfile(obj.Path, obj.filename); 
     video_output_path = fullfile(obj.full_video_path); 
 
-    % Construct the output video 
-    obj.utility_library.dir_to_video(temp_dir_path, video_output_path, obj.FrameRate); 
+    % Check if the temp dir existed, if it did, we remove it, if it did not, we do nothing 
+    if(exist(temp_dir_path, "dir"))
+        % List everything in the directory
+        files = dir(temp_dir_path);
 
-    % Once the video is made, remove the temp dir 
-    rmdir(temp_dir_path, 's');
+        % Remove "." and ".."
+        files = files(~ismember({files.name}, {'.','..'}));
+
+        % If there is a non-zero amount of frames, make the video
+        if(~isempty(files))
+            obj.utility_library.dir_to_video(temp_dir_path, video_output_path, obj.FrameRate); 
+        end 
+
+
+        % Once the video is made, remove the temp dir 
+        rmdir(temp_dir_path, 's');
+    end 
 
 end 
