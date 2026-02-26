d78ccb29b746171ea4f778006586d614713773f9
Zachary Kelly <zjkelly19@gmail.com>
2026-02-26 10:09:40 -0500

    sanity

--- code/applyVirtualFoveation/pythonCode/virtual_foveation.py
+++ code/applyVirtualFoveation/pythonCode/virtual_foveation.py
@@ -1,7 +1,52 @@
 import numpy as np 
 import natsort
 import os 
+import sys
 import matplotlib.pyplot as plt
+import subprocess
+from typing import Literal
+
+egocentric_video_mapper_path: str = os.path.join(os.path.dirname(__file__), "egocentric_video_mapper")
+assert os.path.exists(egocentric_video_mapper_path), "Could not find egocentric video mapper. Ensure it is cloned"
+sys.path.append(egocentric_video_mapper_path)
+from pupil_labs.egocentric_video_mapper.utils import show_videos_preview
+
+import pupil_labs.egocentric_video_mapper.__main__ as egocentric_main
+
+
+class Args:
+    def __init__(self, 
+                 neon_timeseries_dir: str, 
+                 alternative_vid_path: str, 
+                 output_dir: str, 
+                 mapping_choice: Literal["Fixations", "Gaze", "Both"], 
+                 optic_flow_algorithm: Literal["Lucas-Kanade", "Gunnar Farneback"], 
+                 image_matcher: Literal["Efficient_LOFTR", "LOFTR_indoor"],
+                 refresh_time_threshold_sec: float, 
+                 render_video_comparison: bool, 
+                 render_video: bool
+                ):
+        
+        
+        self.neon_timeseries_dir = neon_timeseries_dir
+        self.alternative_vid_path = alternative_vid_path
+        self.output_dir = output_dir
+        self.mapping_choice = mapping_choice
+        self.optic_flow_choice = optic_flow_algorithm
+        self.matcher = image_matcher
+        try:
+            self.refresh_time_thrshld = refresh_time_threshold_sec
+        except NameError:
+            self.refresh_time_thrshld = None
+        try:
+            self.render_comparison_video = render_video_comparison
+        except NameError:
+            self.render_comparison_video = False
+        try:
+            self.render_video = render_video
+        except NameError:
+            self.render_video = False
+
 
 """Find the start times of each of the sensors in the recording"""
 def find_sensor_start_end_times(path_to_recording: str) -> dict[str, tuple]:
@@ -58,6 +103,70 @@ def find_sensor_start_end_times(path_to_recording: str) -> dict[str, tuple]:
         sensor_start_ends[sensor] = (start, end)
 
     return sensor_start_ends
+
+def convert_avi_to_mp4(input_path: str, output_path: str) -> None:
+    cmd = [
+        "ffmpeg",
+        "-y",                     # force overwrite
+        "-i", input_path,
+        "-c:v", "libx264",
+        "-crf", "18",
+        "-preset", "slow",
+        "-c:a", "aac",
+        "-b:a", "192k",
+        output_path
+    ]
+
+    subprocess.run(cmd, check=True)
+
+
+"""
+Wrapper function for the Pupil Labs Egocentric Video Mapper 
+so that one does not need to fill out and run the jupyter notebook 
+every time
+"""
+def run_egocentric_video_mapper(neon_timeseries_dir: str, 
+                                alternative_vid_path: str,  
+                                output_dir: str, 
+                                mapping_choice: Literal["Fixations", "Gaze", "Both"]='Both',
+                                refresh_time_threshold_sec: float=0.5 ,
+                                render_video: bool= False,
+                                render_video_comparison: bool= False,
+                                optic_flow_algorithm: Literal["Lucas-Kanade", "Gunnar Farneback"] = "Lucas-Kanade", 
+                                image_matcher: Literal["Efficient_LOFTR", "LOFTR_indoor"] = "Efficient_LOFTR",
+                                show_video_preview: bool=False
+                               ) -> None:
+
+    # Show a video preview of the two videos if desired 
+    if(show_video_preview is True):
+        show_videos_preview(neon_timeseries_dir, alternative_vid_path)
+
+    # First, we need to convert our video to .mp4 if it is not already in 
+    # that format due to the pupil labs code requiring a .avi video 
+    alternate_vid_dir: str = os.path.dirname(alternative_vid_path)
+    temp_output_path: str = os.path.join(alternate_vid_dir, "temp.mp4")
+    convert_avi_to_mp4(alternative_vid_path, temp_output_path)
+
+    # Package the args for the egocentric video mapper 
+    args: Args = Args(neon_timeseries_dir, 
+                      temp_output_path, 
+                      output_dir, 
+                      mapping_choice, 
+                      optic_flow_algorithm, 
+                      image_matcher,
+                      refresh_time_threshold_sec, 
+                      render_video_comparison, 
+                      render_video
+                    )
+
+    # Run the egocentric video mapper
+    egocentric_main.main(args)
+
+    # Remove the temporary video 
+    os.remove(temp_output_path)
+
+    return 
+
 
 def main():
     pass 
