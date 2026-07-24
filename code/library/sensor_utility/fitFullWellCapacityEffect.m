6adb1d83e59ee2db579f37d8ac689214af7d64c7
Zachary Kelly <zjkelly19@gmail.com>
2026-07-24 13:07:14 -0400

    runs

--- code/library/sensor_utility/fitFullWellCapacityEffect.m
+++ code/library/sensor_utility/fitFullWellCapacityEffect.m
@@ -51,7 +51,7 @@ close all
 dropbox_base_dir = getpref('lightLoggerAnalysis', 'dropboxBaseDir'); 
 
 % Load in the calibration data 
-calibration_data_path = fullfile(dropbox_base_dir, "FLIC_data/LightLoggerRadCal/W1P1M1/fullWellEffectCalibrationData"); 
+calibration_data_path = fullfile(dropbox_base_dir, "FLIC_data/LightLoggerRadCal/W1P1M1/fullWellEffectCalibrationData/camera_linearity_ND0_ND0p4_rgb_means.mat"); 
 load(calibration_data_path)
 data = nd04_rgb_means;
 
