06d475fc8cd1691498999e29ff5ff9b3e625e14a
gkaguirre <gkaguirre@me.com>
2026-08-22 08:37:29 -0400

    Loading the empirical dark signal value

--- code/defineWorldCameraCalibration/utilities/linearizeIMX219SensorCounts.m
+++ code/defineWorldCameraCalibration/utilities/linearizeIMX219SensorCounts.m
@@ -1,14 +1,23 @@
 function yLinear = linearizeIMX219SensorCounts(y, n)
-% This function implements a correction of the soft saturating
-% non-linearity that is explored in the routine
-% defineFullWellCapacityEffect. The function takes raw sensor counts from
-% the IMX219 chip, along with the empirically measured exponent parameter,
-% and returns the linearized value. The routine has hard-coded the minimum
-% dark value and the bit-depth of the camera. The linearized values are
-% returned as floats.
+% This function implements a correction of the soft saturating non-
+% linearity that is explored in the routine defineFullWellCapacityEffect.
+% The function takes raw sensor counts from the IMX219 chip, along with the
+% empirically measured exponent parameter, and returns the linearized
+% value. The routine has hard-coded the minimum dark value and the
+% bit-depth of the camera. The linearized values are returned as floats.
 
-% Define the minimum dark value
-Smin = 8*2;
+% The "dark signal" value is the empirically measured sensor value reported
+% by the camera under conditions of zero true photon capture. We load this
+% value into a persistent variable.
+persistent darkSignal
+if isempty(darkSignal)
+    paramFileName = fullfile(...
+        tbLocateProjectSilent('lightLoggerAnalysis'),...
+        'derived',...
+        'darkSignal.mat');
+    load(paramFileName,'darkSignal');
+end
+Smin = darkSignal;
 
 % Define the fixed asymptotic value
 Smax = 2^8-1 - Smin;
